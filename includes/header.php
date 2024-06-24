3aee3a263e2f2483caf87212ae7da2cd9ad9d08c
aimeennnn <muh.aiman21304@gmail.com>
2024-06-24 19:21:56 +0700

    Update = Perubahan File

--- includes/header.php
+++ includes/header.php
@@ -10,13 +10,18 @@
     <header>
         <h1>Arsip Surat</h1>
         <nav>
-            <a href="/index.php">Home</a>
             <?php
-            session_start();
+            // Start the session if not already started
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
+
             if (isset($_SESSION['username'])) {
+                echo '<a href="/index.php">Home</a>';
                 echo '<a href="/views/add_letter.php">Tambah Surat</a>';
                 echo '<a href="/logout.php">Logout (' . $_SESSION['username'] . ')</a>';
             } else {
+                echo '<a href="/index.php">Home</a>';
                 echo '<a href="/views/login.php">Login</a>';
                 echo '<a href="/views/register.php">Register</a>';
             }
